cbbf96eb6033d4f454b7c8c56214d3fa7349927c
bobzhu-11 <bobzhude@gmail.com>
2025-04-22 22:31:18 +0200

    Wobble Water

---
+++ UE_PlaneGame/Source/UE_PlaneGame/Private/Items/WobbleWater.cpp
@@ -0,0 +1,63 @@
+﻿
+#include "Items/WobbleWater.h"
+
+
+AWobbleWater::AWobbleWater()
+{
+
+    PrimaryActorTick.bCanEverTick = true;
+
+
+    MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+    RootComponent = MeshComponent;
+}
+
+
+void AWobbleWater::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    if (MeshComponent->GetMaterial(0))
+    {
+        DynamicMaterial = MeshComponent->CreateAndSetMaterialInstanceDynamic(0);
+    }
+    
+    LastPosition = GetActorLocation();
+    LastRotation = GetActorRotation();
+}
+
+void AWobbleWater::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    
+    if (!DynamicMaterial)
+        return;
+    
+    Time += DeltaTime;
+    
+    WobbleAmountToAddX = FMath::Lerp(WobbleAmountToAddX, 0.0f, DeltaTime * Recovery);
+    WobbleAmountToAddZ = FMath::Lerp(WobbleAmountToAddZ, 0.0f, DeltaTime * Recovery);
+    
+    Pulse = 2 * PI * WobbleSpeed;
+    WobbleAmountX = WobbleAmountToAddX * FMath::Sin(Pulse * Time);
+    WobbleAmountZ = WobbleAmountToAddZ * FMath::Sin(Pulse * Time);
+    
+    DynamicMaterial->SetScalarParameterValue(FName("WobbleX"), WobbleAmountX);
+    DynamicMaterial->SetScalarParameterValue(FName("WobbleZ"), WobbleAmountZ);
+    
+    FVector CurrentPosition = GetActorLocation();
+    Velocity = (CurrentPosition - LastPosition) / DeltaTime;
+    
+    FRotator CurrentRotation = GetActorRotation();
+    AngularVelocity = FVector(
+        FMath::UnwindDegrees(CurrentRotation.Pitch - LastRotation.Pitch),
+        FMath::UnwindDegrees(CurrentRotation.Yaw - LastRotation.Yaw),
+        FMath::UnwindDegrees(CurrentRotation.Roll - LastRotation.Roll)
+    );
+
+    WobbleAmountToAddX += FMath::Clamp((Velocity.X + (AngularVelocity.Z * 0.2f)) * MaxWobble, -MaxWobble, MaxWobble);
+    WobbleAmountToAddZ += FMath::Clamp((Velocity.Z + (AngularVelocity.X * 0.2f)) * MaxWobble, -MaxWobble, MaxWobble);
+    
+    LastPosition = CurrentPosition;
+    LastRotation = CurrentRotation;
+}
---
+++ UE_PlaneGame/Source/UE_PlaneGame/Public/Items/WobbleWater.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include <Interactables/Items/WorldItem.h>
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Items/WorldItem.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include "WobbleWater.generated.h"
+
+UCLASS()
+class UE_PLANEGAME_API AWobbleWater :AWorldItem
+{
+	GENERATED_BODY()
+    
+public:    
+
+	AWobbleWater();
+	
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+    
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComponent;
+	
+	UPROPERTY()
+	UMaterialInstanceDynamic* DynamicMaterial;
+	
+	UPROPERTY(EditAnywhere, Category = "Wobble")
+	float MaxWobble = 0.03f;
+    
+	UPROPERTY(EditAnywhere, Category = "Wobble")
+	float WobbleSpeed = 1.0f;
+    
+	UPROPERTY(EditAnywhere, Category = "Wobble")
+	float Recovery = 1.0f;
+	
+	FVector LastPosition;
+	FVector Velocity;
+	FRotator LastRotation;
+	FVector AngularVelocity;
+    
+	float WobbleAmountX;
+	float WobbleAmountZ;
+	float WobbleAmountToAddX;
+	float WobbleAmountToAddZ;
+	float Pulse;
+	float Time = 0.5f;
+};
